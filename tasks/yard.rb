9454648aa38e1370ef82390d2467bdcc1d454d3b
postmodern <postmodern.mod3@gmail.com>
2009-06-06 17:45:15 -0700

    Updated the files list.

--- tasks/yard.rb
+++ tasks/yard.rb
@@ -1,7 +1,7 @@
 require 'yard'
 
 YARD::Rake::YardocTask.new do |t|
-  t.files   = ['lib/**/*.rb']
+  t.files   = ['lib/**/*.rb', 'History.txt', 'TODO.txt', 'README.txt']
 end
 
 task :docs => :yardoc
