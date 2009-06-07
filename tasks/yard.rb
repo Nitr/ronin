1289f1ee0142dbbe92c3f68c4bbfa78fa38fbd33
postmodern <postmodern.mod3@gmail.com>
2009-06-06 18:05:09 -0700

    Show protected methods.

--- tasks/yard.rb
+++ tasks/yard.rb
@@ -2,6 +2,7 @@ require 'yard'
 
 YARD::Rake::YardocTask.new do |t|
   t.files   = ['lib/**/*.rb', 'History.txt', 'TODO.txt', 'README.txt']
+  t.options = ['--protected']
 end
 
 task :docs => :yardoc
